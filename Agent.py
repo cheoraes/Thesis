3063d4b226e0b88081894aa719e68f0ab8b69500
Jordi <cheoraes@gmail.com>
2020-03-28 23:11:36 +0100

    Inicio el agente muestra datos personales selecciona una presa al azahar excribe en el terminal con colores offer sex get sex

--- Agent.py
+++ Agent.py
@@ -1,22 +1,26 @@
 import random
+import json
 from names import get_first_name
 
 from termcolor import colored, cprint
 
 class Agent:
     prey = None
-    def __init__(self, gender, SMV):
+    hunters = []
+    def __init__(self, gender, SMV, population):
+        self.male_population_description = population[0]
+        self.female_population_description = population[1]
         self.gender = gender
         self.SMV = SMV
         self.name = get_first_name(gender=self.gender)
+        #print(json.dumps(self.male_population_description, indent=4))
         print(colored("Creation:", 'green'), self.personal_data(self))
 
 
-    def personal_data(self,agent):
+    def personal_data(self, agent):
         color = 'blue' if agent.gender == "male" else 'magenta'
         attributes = []
-
-        return colored( agent.name + " " + agent.gender + " " + str(agent.SMV), color, attrs = attributes)
+        return colored(agent.name + " " + agent.gender + " " + str(agent.SMV), color, attrs=attributes)
 
 
 
@@ -24,14 +28,38 @@ class Agent:
 
     def explore(self, population):
         color='grey'
-        attributes=['reverse']
+        attributes=['bold']
         candidates = list(filter(lambda obj: obj.gender != self.gender, population))
         self.prey = random.choice(candidates)
-        print(self.personal_data(self), colored("runs investigation on Prey:",color, attrs = attributes), self.personal_data(self.prey))
-        self.offerSex()
+        print(self.personal_data(self), colored("runs investigation on Prey:",color, attrs=attributes), self.personal_data(self.prey))
+
 
     def offerSex(self):
         color = 'yellow'
         attributes = ['bold']
-        if self.prey != None :
-            print(self.personal_data(self), colored("offers sex to ",color,attrs = attributes), self.personal_data(self.prey))
+        if self.prey is not None:
+            self.prey.getSexOffer(self);
+            print(self.personal_data(self), colored("offers sex to ",color,attrs=attributes), self.personal_data(self.prey))
+
+    def getSexOffer(self,agent):
+        color = 'cyan'
+        attributes = ['bold']
+        self.hunters.append(agent)
+        print(self.personal_data(self), colored("gets Sex Offer from ", color, attrs=attributes),
+              self.personal_data(agent))
+        print("\thunter list")
+        for hunter in self.hunters:
+            print("\t\t" + self.personal_data(hunter))
+
+    def acceptBestSexOffer(self):
+        color = 'green'
+        attributes = ['bold']
+        if len(self.hunters) > 0:
+            self.hunters.sort(key=lambda obj: obj.SMV, reverse=True)
+            sexpartner = self.hunters.pop(0)
+            print(self.personal_data(self),colored("accept Best Sex Offer with ",color,attrs=attributes), self.personal_data(sexpartner) )
+            self.hunters = []
+            print("\thunter list empty")
+
+
+
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys
 import ast
+import json
 import math
 import statistics
 import numpy as np
 import scipy.stats
 import pandas as pd
+import random
 from matplotlib import pyplot as plt
+
 from Agent import Agent
 
 
@@ -15,13 +18,7 @@ male_SMVs = ast.literal_eval(sys.argv[1])
 female_SMVs = ast.literal_eval(sys.argv[2])
 
 
-#Agents creation
-Agents = []
-for i in male_SMVs:
-    Agents.append(Agent(gender='male', SMV=i))
-for i in female_SMVs:
-    Agents.append(Agent(gender='female', SMV=i))
-'''
+
 #Population Description
 print("\nPopulation Description")
 print("Males")
@@ -30,34 +27,26 @@ male_population_description = {
     "count":len(male_SMVs),
     "min":min(male_SMVs),
     "max":max(male_SMVs),
+    "ratio":len(male_SMVs)/(len(female_SMVs)+len(male_SMVs)),
     "mean":statistics.mean(male_SMVs),
     "hmean":statistics.harmonic_mean(male_SMVs),
     "gmean":statistics.geometric_mean(male_SMVs),
 }
-print("\tPopulation:", male_population_description["population"])
-print("\tCount:", male_population_description["count"])
-print("\tMin:", male_population_description["min"])
-print("\tMax:", male_population_description["max"])
-print("\tMean:", male_population_description["mean"])
-print("\tHarmonic Mean:", male_population_description["hmean"])
-print("\tGeometric Mean:", male_population_description["gmean"])
+
+print(json.dumps(male_population_description, indent=4))
+
 print("Female")
 female_population_description = {
     "population":female_SMVs,
     "count":len(female_SMVs),
     "min":min(female_SMVs),
     "max":max(female_SMVs),
+    "ratio":len(female_SMVs)/(len(female_SMVs)+len(male_SMVs)),
     "mean":statistics.mean(female_SMVs),
     "hmean":statistics.harmonic_mean(female_SMVs),
     "gmean":statistics.geometric_mean(female_SMVs),
 }
-print("\tPopulation:", female_population_description["population"])
-print("\tCount:", female_population_description["count"])
-print("\tMin:", female_population_description["min"])
-print("\tMax:", female_population_description["max"])
-print("\tMean:", female_population_description["mean"])
-print("\tHarmonic Mean:", female_population_description["hmean"])
-print("\tGeometric Mean:", female_population_description["gmean"])
+print(json.dumps(female_population_description, indent=4))
 
 
 
@@ -65,8 +54,12 @@ bins = np.arange(min(male_population_description["min"], female_population_descr
 plt.hist([male_population_description["population"],
          female_population_description["population"]],
          bins=bins, color=['blue', 'pink'], edgecolor='black', rwidth=1.0,
-         label=["Male ("+str(male_population_description["count"])+") ratio: "+ str(round(male_population_description["count"]/(male_population_description["count"]+female_population_description["count"]),2)),
-                "Female ("+str(female_population_description["count"])+") ratio: "+ str(round(female_population_description["count"]/(male_population_description["count"]+female_population_description["count"]),2))])
+         label=["Male ("+str(male_population_description["count"]) +
+                ") ratio: " + str(round(male_population_description["ratio"], 2)),
+                "Female ("+str(female_population_description["count"]) +
+                ") ratio: " + str(round(female_population_description["ratio"], 2))])
+
+
 
 plt.axvline(female_population_description["mean"], color='red', label='F Mean')
 plt.axvline(female_population_description["hmean"], color='yellow', label='F Harm Mean')
@@ -78,7 +71,21 @@ plt.legend()
 plt.title('Population Description')
 plt.xlabel('SMV')
 plt.ylabel('#Agents')
-plt.show()
-'''
+#plt.show()
+
+
+#Agents creation
+Agents = []
+for i in male_SMVs:
+    Agents.append(Agent(gender='male', SMV=i, population = [male_population_description,female_population_description]))
+for i in female_SMVs:
+    Agents.append(Agent(gender='female', SMV=i, population = [male_population_description,female_population_description]))
+
 
 Agents[0].explore(Agents)
+Agents[1].explore(Agents)
+Agents[2].explore(Agents)
+Agents[0].offerSex()
+Agents[1].offerSex()
+Agents[2].offerSex()
+Agents[3].acceptBestSexOffer()
